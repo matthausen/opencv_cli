3dd821219d3c679fc7eea2128b6154683ab0697f
matthausen <matteo.fusilli@hotmail.it>
2021-07-26 19:11:40 +0100

    first commit

---
+++ lib/facedetection.h
@@ -0,0 +1,14 @@
+//
+// Created by Matteo Fusilli on 26/07/2021.
+//
+
+#ifndef CPP_CMD_OPENCV_FACEDETECTION_H
+#define CPP_CMD_OPENCV_FACEDETECTION_H
+
+
+class FaceDetection {
+
+};
+
+
+#endif //CPP_CMD_OPENCV_FACEDETECTION_H
---
+++ lib/virtualpainter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Fusilli on 26/07/2021.
+//
+
+#include "virtualpainter.h"
